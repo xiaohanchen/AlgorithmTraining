55c8834425f9d22178ff32a7ee9afadc6ac909f1
SensitiveOnion <cxhdtcvvv@gmail.com>
2018-05-09 00:42:10 +0100

    9 May

---
+++ src/CrackInterview/BitOperation.java
@@ -0,0 +1,4 @@
+package CrackInterview;
+
+public class BitOperation {
+}
---
+++ src/CrackInterview/OnlineBookStore.java
@@ -0,0 +1,63 @@
+package MultiThreading;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+interface Store{
+    void checkout();
+    void order();
+}
+
+abstract class Book{
+    String name;
+    String author;
+    double price;
+    int stockCount;
+    ArrayList<BookCategory> categories;
+
+    //....
+}
+
+enum BookCategory{
+    FICTION, MANGA  //....
+}
+
+public class OnlineBookStore implements Store{
+
+    BookStock bookStock;
+    ShopCart shopCart;
+    BookSearcher bookSearcher;
+
+    @Override
+    public void checkout() {
+
+    }
+
+    @Override
+    public void order() {
+
+    }
+
+    void checkBook(){
+
+    }
+
+}
+
+
+class ShopCart{
+
+}
+
+
+class BookStock{
+    HashMap<Book, Integer> bookStock;  //category -> Books
+}
+
+
+class BookSearcher{
+
+}
+
+
+
